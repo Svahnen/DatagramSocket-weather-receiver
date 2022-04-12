6a4bb6a960a600997a7ef8ee4ec4e6be8f55d441
Svahnen <svahn.jonny@gmail.com>
2022-04-12 14:50:42 +0200

    Receive messages as new labels in grid layout

--- src/GUI.java
+++ src/GUI.java
@@ -1,25 +1,36 @@
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class GUI extends JFrame {
     DataReceiver receiver;
+    List<JLabel> messageLabels = new ArrayList<JLabel>();
+    JPanel panel1 = new JPanel();
 
-    public GUI(DataReceiver receiver) {
+    public GUI(DataReceiver receiver) throws UnknownHostException, SocketException, IOException {
         this.receiver = receiver;
+
+        panel1.setLayout(new GridLayout(20, 1));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
+        setSize(300, 400);
+        setLocationRelativeTo(null);
         setVisible(true);
-        try {
-            receiver.getData();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        add(panel1);
+        panel1.add(new JLabel("test"));
+
+        while (true) {
+            messageLabels.add(new JLabel(receiver.getData()));
+            panel1.add(messageLabels.get(messageLabels.size() - 1));
+            revalidate();
+            repaint();
         }
     }
 }
